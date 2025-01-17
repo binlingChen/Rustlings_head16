d8bcb9e49d8ac2b1f3bdadcd20a0ecb118da3dd5
binlingChen <2824888433@qq.com>
2025-01-17 23:51:51 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM  DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num <= 40{
+        num * 2
+    } else {
+        num * 1
+
+
+    }
+}
+
 
 // Don't modify this function!
 #[test]
